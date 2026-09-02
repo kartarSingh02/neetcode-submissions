ae8a69316042213691921360df00d4cfc2e10c4b
Kartar Singh <73885354+kartarSingh02@users.noreply.github.com>
2026-09-02 23:27:26 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        int m = s1.length();
+        int n = s2.length();
+
+        if(m > n) return false;
+
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for(int i = 0; i < m; i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+
+        if(freq1 == freq2)
+            return true;
+
+        for(int i = m; i < n; i++) {
+
+            // add new character
+            freq2[s2[i] - 'a']++;
+
+            // remove character leaving window
+            freq2[s2[i-m] - 'a']--;
+
+            if(freq1 == freq2)
+                return true;
+        }
+
+        return false;
+    }
+};
